c6abc215b7accd469fbfd7c6c983d2fba547eab4
Niko Strijbol <strijbol.niko@gmail.com>
2020-02-05 15:21:25 +0100

    Update dependencies and enable all tests

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule UeberauthCAS.Mixfile do
   use Mix.Project
 
-  @version "1.1.0"
+  @version "1.2.0-fork"
   @url     "https://github.com/marceldegraaf/ueberauth_cas"
 
   def project do
@@ -29,15 +29,15 @@ defmodule UeberauthCAS.Mixfile do
 
   defp deps do
     [
-      {:ueberauth, "~> 0.4"},
-      {:httpoison, "~> 0.13"},
-      {:sweet_xml, "~> 0.6.6"},
-      {:yaml_elixir, "~> 2.4.0"},
-      {:excoveralls, "~> 0.12.0", only: :test},
-      {:inch_ex, "~> 1.0.1", only: :docs},
-      {:earmark, "~> 1.4.2", only: :dev},
-      {:ex_doc, "~> 0.21.2", only: :dev},
-      {:mock, "~> 0.3.3", only: :test},
+      {:ueberauth, "~> 0.6"},
+      {:httpoison, "~> 1.6"},
+      {:sweet_xml, "~> 0.6"},
+      {:yaml_elixir, "~> 2.4"},
+      {:excoveralls, "~> 0.12", only: :test},
+      {:inch_ex, "~> 2.0", only: :docs},
+      {:earmark, "~> 1.4", only: :dev},
+      {:ex_doc, "~> 0.21", only: :dev},
+      {:mock, "~> 0.3", only: :test},
     ]
   end
 
